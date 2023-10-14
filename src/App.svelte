d632b2a5e7aaa75dbfab8432ee66a58be0ecba9b
Valerian Coelho <valerian27012004@gmail.com>
2023-10-14 21:07:38 +0530

    Notice Component Added

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import Lecture from "./lib/Lecture.svelte";
+  import Notice from "./lib/Notice.svelte";
   import PerformanceCard from "./lib/PerformanceCard.svelte";
 </script>
 
 <main>
   <Lecture fromTime='9:00' toTime='10:00' subjectName='Database Management and Query Processing' className='LHM2'/>
   <PerformanceCard/>
+  <Notice date='15th August 2023' text='Celebration of Independence Day'/>
 </main>
 
 <style>
---
+++ src/lib/Notice.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let date;
+    export let text;
+  </script>
+  
+  <div class="notice-data">
+    <div class="date">{date}</div>
+    <div class="text">{text}</div>
+  </div>
+  
+  <style>
+    .notice-data {
+      background-color: rgba(205, 205, 205, 0.20);
+      display: inline-block;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid rgba(0, 0, 0, .2);
+      font-weight: 600;
+    }
+    .date {
+      margin-bottom: 20px;
+    }
+  </style>
