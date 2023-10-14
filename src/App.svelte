3234c078454c44aaec1ed970488480f68a7bd54e
Valerian Coelho <valerian27012004@gmail.com>
2023-10-14 17:20:28 +0530

    Performance Card Complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,17 @@
 <script>
   import Lecture from "./lib/Lecture.svelte";
+  import PerformanceCard from "./lib/PerformanceCard.svelte";
 </script>
 
 <main>
   <Lecture fromTime='9:00' toTime='10:00' subjectName='Database Management and Query Processing' className='LHM2'/>
+  <PerformanceCard/>
 </main>
 
 <style>
-  *{
+  :global(html, body) {  
+    margin: 0;
+    padding: 0;
     font-family: Arial, Helvetica, sans-serif;
   }
 </style>
---
+++ src/lib/PerformanceCard.svelte
@@ -0,0 +1,28 @@
+<script>
+  import DonutChart from "./DonutChart.svelte";
+
+
+</script>
+
+<div class="performance-card">
+    <h2 class="title">Marks</h2>
+    <h5 class="sub-title">Internal Test 1</h5>
+    <DonutChart/>
+</div>
+
+<style>
+    .performance-card, .title, .sub-title {
+        margin: 0;
+        padding: 0;
+    }
+    .performance-card {
+        border: 1px solid rgba(0, 0, 0, .2);
+        display: inline-block;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    .sub-title {
+        color: gray;
+        margin-bottom: 20px;
+    }
+</style>
