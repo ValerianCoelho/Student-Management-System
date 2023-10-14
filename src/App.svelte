334a150b1e3911afca83b12d90180cdd2733d209
Valerian Coelho <valerian27012004@gmail.com>
2023-10-14 16:08:34 +0530

    Lecture Component Created

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Lecture from "./lib/Lecture.svelte";
+</script>
+
+<main>
+  <Lecture fromTime='9:00' toTime='10:00' subjectName='Database Management and Query Processing' className='LHM2'/>
+</main>
+
+<style>
+  *{
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
