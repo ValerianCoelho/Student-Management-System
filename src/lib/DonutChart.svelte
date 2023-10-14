cb39031874cfd04e6e9788d27b60b3c729faf9d9
Valerian Coelho <valerian27012004@gmail.com>
2023-10-14 17:05:04 +0530

    Donut Chart Component Complete

---
+++ src/lib/DonutChart.svelte
@@ -0,0 +1,27 @@
+<script>
+    let completed = '10%';
+</script>
+
+<div class="donut-chart">
+    <div class="outer">
+        <div class="inner"></div>
+    </div>
+</div>
+
+<style>
+    .outer {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        background: conic-gradient(#0075FF 90%, #d9e3f8 90%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .inner {
+        width: 60%;
+        height: 60%;
+        background-color: white;
+        border-radius: 50%;
+    }
+</style>
