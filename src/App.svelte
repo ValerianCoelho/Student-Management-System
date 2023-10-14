8f76380d135a8d8e4fb7ae13a5cdcf6349a0831a
Valerian Coelho <valerian27012004@gmail.com>
2023-10-14 21:43:46 +0530

    Table Component Added

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,24 @@
   import Lecture from "./lib/Lecture.svelte";
   import Notice from "./lib/Notice.svelte";
   import PerformanceCard from "./lib/PerformanceCard.svelte";
+  import Table from "./lib/Table.svelte";
 </script>
 
 <main>
-  <Lecture fromTime='9:00' toTime='10:00' subjectName='Database Management and Query Processing' className='LHM2'/>
+  <!-- <Lecture fromTime='9:00' toTime='10:00' subjectName='Database Management and Query Processing' className='LHM2'/>
   <PerformanceCard/>
   <HorizontalPerformanceCard completedColor='#0075FF' remainingColor='#d9e3f8' message1='24 Lectures Attended' message2='6 Lectures Missed'/>
-  <Notice date='15th August 2023' text='Celebration of Independence Day'/>
+  <Notice date='15th August 2023' text='Celebration of Independence Day'/> -->
+  <Table tableData={[
+    ['Subject', 'Attendance', 'Status'],
+    ['DBMQ', '80%', 'Passed'],
+    ['OS', '90%', 'Passed'],
+    ['OOPJ', '100%', 'Passed'],
+    ['Math', '75%', 'Failed'],
+    ['History', '88%', 'Passed'],
+    ['English', '95%', 'Passed']
+]}/>
+
 </main>
 
 <style>
---
+++ src/lib/Table.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let tableData;
+</script>
+
+<table class="table">
+    {#each tableData as row, rowIndex}
+        <tr>
+            {#each row as cell}
+                {#if rowIndex == 0}
+                    <th class="header">{cell}</th>
+                {:else}
+                    <td class="data">{cell}</td>
+                {/if}
+            {/each}
+        </tr>
+    {/each}
+</table>
+
+<style>
+    .table {
+        border-collapse: collapse;
+        outline: 1px solid #ccc;
+        border-radius: 10px;
+        overflow: hidden;
+    }
+    
+    .header {
+        font-weight: bold;
+        background-color: rgba(0, 70, 207, 0.10);
+        padding: 8px;
+    }
+
+    .data {
+        border-top: 1px solid #ccc;
+        padding: 8px;
+    }
+</style>
