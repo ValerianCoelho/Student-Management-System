f32dec9962762d43e882213081ffb9daf47a243a
Valerian Coelho <valerian27012004@gmail.com>
2023-10-16 23:20:49 +0530

    Donut Chart Percentage Added

--- src/lib/Components/DonutChart.svelte
+++ src/lib/Components/DonutChart.svelte
@@ -7,7 +7,9 @@
 
 <div class="donut-chart">
   <div class="outer"  style:width="{size}px" style:height="{size}px" style:background="conic-gradient({completedColor} {completed}%, {remainingColor} {completed}%)" style:border-radius="50%">
-    <div class="inner"></div>
+    <div class="inner">
+      <div class="percentage-text">{completed}%</div>
+    </div>
   </div>
 </div>
 
@@ -16,6 +18,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
   }
   .inner {
     width: 60%;
@@ -23,4 +26,18 @@
     background-color: white;
     border-radius: 50%;
   }
+  .percentage-text {
+    position: absolute;
+    top: -2%;
+    right: 8%;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid rgb(209, 209, 209);
+    display: inline;
+    font-weight: 900;
+    background-color: rgba(255, 255, 255, 0.2); /* Semi-transparent background */
+    backdrop-filter: blur(10px); /* Blur effect */
+    font-size: .7em;
+}
+
 </style>
