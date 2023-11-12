9d7b7b58a0273c623dcb28521229d0a55d969d7b
Valerian Coelho <valerian27012004@gmail.com>
2023-11-12 22:05:18 +0530

    Bugs Fixed

--- Faculty/Faculty Insert/facInsert.php
+++ Faculty/Faculty Insert/facInsert.php
@@ -12,9 +12,10 @@
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
     $email = $_POST["email"];
+    $subject = $_POST["subject"];
     $pswd = $_POST["password"];
 
-    $q1 = "insert into faculty (faculty_id, fname, lname, email, password) values ('$st_id', '$fname', '$lname', '$email', '$pswd')";
+    $q1 = "insert into faculty (faculty_id, fname, lname, email, subject, password) values ('$st_id', '$fname', '$lname', '$email', '$subject', '$pswd')";
     $r1 = mysqli_query($conn, $q1);
     if($r1)
         echo "<br><br>Values inserted successfully";
